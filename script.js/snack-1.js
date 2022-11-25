9412b9937f7c3220ffd5ad94211c8bd2b8298e41
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-25 16:28:24 +0100

    somma numeri snack-3

--- script.js/snack-1.js
+++ script.js/snack-1.js
@@ -1,27 +1,17 @@
-// Snack 1#####################################
+// Snack 1n#####################################
 
-// const lista = [ ];
+const lista = [ ];
 
-// for ( i = 0; i < 6; i++){
+for ( i = 0; i < 6; i++){
     
-//     const numero = parseInt(prompt('Inserisci numero'));
+    const numero = parseInt(prompt('Inserisci numero'));
 
-//     if (numero % 2 == 1){
-//         console.log("pari")
-//         // inserisco nell'array
-//         lista.push(numero);
-//     } 
-// }
-
-// console.log(lista)
-
-// Snack 2########################################
-
-
-const inputNumero = parseInt(prompt('Inserici un numero'));
-
-if (inputNumero % 2 == 0){
-    console.log(inputNumero);
-} else {
-    console.log (inputNumero + 1)
+    if (numero % 2 == 1){
+        console.log("pari")
+        // inserisco nell'array
+        lista.push(numero);
+    } 
 }
+
+console.log(lista)
+
---
+++ script.js/snack-3.js
@@ -0,0 +1,9 @@
+const listaNumeri = [1,2,3,4,5,6,7,8,9,10,3,6,78,3,4,5];
+
+somma = 0;
+
+for (i = 0; i < 10; i++){
+    somma += listaNumeri[i];
+}
+
+console.log(somma)

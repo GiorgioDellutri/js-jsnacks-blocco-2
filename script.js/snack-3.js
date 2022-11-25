2c8c49b4c2b79ffd3deccaee642e869cafcd8ded
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-25 16:58:09 +0100

    snack-3 pt 2 rev

--- script.js/snack-3.js
+++ script.js/snack-3.js
@@ -2,10 +2,11 @@ const listaNumeri = [1,2,3,4,5,6,7,8,9,10,3,6,78,3,4,5];
 
 somma = 0;
 let media;
+
 for (i = 0; i < 10; i++){
     somma += listaNumeri[i];
-    media = somma / 10;
 }
+media = somma / 10;
 
-console.log("la somma dei primi 10 numeri è" + somma);
+console.log("la somma dei primi 10 numeri è " + somma);
 console.log ("la media è " + media);

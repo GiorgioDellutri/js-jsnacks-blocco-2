a1ac81cdd01f56ddecc00a1bf42579a0000b3b65
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-25 16:08:14 +0100

    snack

---
+++ script.js/script.js
@@ -0,0 +1,28 @@
+// Snack 1
+
+// const lista = [ ];
+
+// for ( i = 0; i < 6; i++){
+    
+//     const numero = parseInt(prompt('Inserisci numero'));
+
+//     if (numero % 2 == 1){
+//         console.log("pari")
+//         // inserisco nell'array
+//         lista.push(numero);
+//     } 
+// }
+
+// console.log(lista)
+
+
+// Snack 2
+
+
+const inputNumero = parseInt(prompt('Inserici un numero'));
+
+if (inputNumero % 2 == 0){
+    console.log(inputNumero);
+} else {
+    console.log (inputNumero + 1)
+}

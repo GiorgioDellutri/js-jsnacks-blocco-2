36d37600ef11d7d9e95dd1e79276d807c371ebc7
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-25 16:21:27 +0100

    seconda risolusione snack 2

--- script.js/snack-2.js
+++ script.js/snack-2.js
@@ -1,10 +1,17 @@
 // Snack 2
-
-
 const inputNumero = parseInt(prompt('Inserici un numero'));
 
 if (inputNumero % 2 == 0){
+    // stampa il numero
     console.log(inputNumero);
 } else {
+    // stampa il successivo
     console.log (inputNumero + 1)
 }
+
+// ALTRA SOLUZIONE
+
+// // stampo solo  se il numero e' dispari incrementando con inputNumero ++ 
+// if (inputNumero % 2 == 1){
+//     inputNumero++
+// }

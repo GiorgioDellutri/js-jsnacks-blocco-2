2a4c0db92d4c4804511ea2a0458a9b242b27d68e
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-28 11:00:27 +0100

    snack-3

--- script.js/snack-3.js
+++ script.js/snack-3.js
@@ -1,12 +1,16 @@
-const listaNumeri = [1,2,3,4,5,6,7,8,9,10,3,6,78,3,4,5];
+const listaNumeri = [1,2,3,46,5,22,7,13,9,10,3,6,78,3,4,5];
 
 somma = 0;
 let media;
+let vmax = listaNumeri[0]
 
 for (i = 0; i < 10; i++){
     somma += listaNumeri[i];
+    if (listaNumeri[i] > vmax){
+    }
 }
 media = somma / 10;
 
 console.log("la somma dei primi 10 numeri è " + somma);
 console.log ("la media è " + media);
+console.log ("Il valore massimo dei primi 10 numeri e'" + vmax)
